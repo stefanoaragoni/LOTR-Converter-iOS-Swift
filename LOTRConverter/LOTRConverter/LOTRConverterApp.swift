9ffe4db0be175a3481bac66dffa75fcbcd70b1ad
Stefano <69864405+stefanoaragoni@users.noreply.github.com>
2023-11-26 17:41:27 -0600

    Initial commit

---
+++ LOTRConverter/LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Stefano Aragoni on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
